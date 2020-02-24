9f91f6691fcb6ac8f4e2a534e2b2a947cbaa46b1
Trevor <10884500@my.uvu.edu>
2020-02-24 16:09:29 -0700

    message

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ document.querySelector('#company').innerText = "Trevor's Bed and Breakfast"
 
 document.querySelector('#slogan').innerText = "Best B&B in Portland, Oregon!"
 
+let userName = prompt("What is your name?")
+
+let message = "Hello " + userName + ", welcome to the future of B&B!"
+
+document.querySelector('#greeting').innerText = message
