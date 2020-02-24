5ffe7dcc78f1f64247cb2264342d06773b129940
Trevor <10884500@my.uvu.edu>
2020-02-24 16:12:00 -0700

    names

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.querySelector('#slogan').innerText = "Best B&B in Portland, Oregon!"
 
 let userName = prompt("What is your name?")
 
-let message = "Hello " + userName + ", welcome to the future of B&B!"
+// let message = "Hello " + userName + ", welcome to the future of B&B!"
+
+let message  = `Hello ${userName}, welcome to the future of B&B!`
 
 document.querySelector('#greeting').innerText = message
